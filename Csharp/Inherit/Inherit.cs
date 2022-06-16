fa73c08dc87bb9df3ece328224ee9981513e363d
Hoshi03 <didqocn114@hanyang.ac.kr>
2022-06-16 21:31:26 +0900

    상속성

--- Csharp/Inherit/Inherit.cs
+++ Csharp/Inherit/Inherit.cs
@@ -4,9 +4,47 @@ namespace Constructor
 {
     class Inherit
     {
+
+        class Player
+        {
+            static public int counter = 1; // 카운터는 하나만 존재
+            public int id = 1;
+            public int hp;
+            public int atk;
+
+            public Player()
+            {
+                Console.WriteLine("플레이어 생성자 호출");
+            }
+
+            public Player(int hp)
+            {
+                this.hp = hp;
+                Console.WriteLine("플레이어 hp 생성자 호출");
+            }
+        }
+
+        class Knight : Player
+        {
+            public Knight() : base(100)// 
+            {
+                Console.WriteLine("knight 생성자 호출");
+            }
+        }
+
+        class Archer : Player
+        {
+
+        }
+        class Mage : Player
+        {
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Knight knight = new Knight();
         }
+
     }
 }

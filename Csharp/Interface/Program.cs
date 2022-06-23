8ad46a2ea1321ec2d67ea7c9ff5ddb9accc5d0d2
Hoshi03 <didqocn114@hanyang.ac.kr>
2022-06-23 15:12:32 +0900

    추상 클래스, 인터페이스

---
+++ Csharp/Interface/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        abstract class Monster
+        {
+            public abstract void Shout();
+        }
+
+        class Orc: Monster
+        {
+            public override void Shout()
+            {
+                Console.WriteLine("록타");
+            }
+        }
+
+        interface IFlyable
+        {
+            void Fly();
+        }
+
+        class FlyableOrc : Orc, IFlyable
+        {
+            public void Fly()
+            {
+
+            }
+        }
+
+        //인터페이스 IFlyable을 가진 클래스에선 사용 가능한 함수
+        static void Dofly(IFlyable flying)
+        {
+            flying.Fly();
+        }
+        static void Main(string[] args)
+        {
+            IFlyable orc = new FlyableOrc();
+            Dofly(orc);
+        }
+    }
+}

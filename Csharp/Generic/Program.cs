32fef1b797b15d760ae90f5798cb868514d888c0
Hoshi03 <didqocn114@hanyang.ac.kr>
2022-06-23 15:11:01 +0900

    object 타입과 일반화

---
+++ Csharp/Generic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Generic
+{
+    class Program
+    {
+
+        class Mylist<T>
+        {
+            T[] arr = new T[10];
+        }
+
+        static void Test<T>(T input)
+        {
+            Console.WriteLine(input);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Mylist<int> a = new Mylist<int>();
+            Mylist<int> b = new Mylist<int>();
+            Test<int>(3/2);
+            Test<float>(3.141592f/2);
+            Test<double>(3.141592/2);
+
+        }
+    }
+}

e26ad559962617e8ddc6fbac48ae62ce53c39867
Hoshi03 <didqocn114@hanyang.ac.kr>
2022-06-23 15:08:50 +0900

    딕셔너리

---
+++ Csharp/Dic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Dic
+{
+    class Monster
+    {
+        public int id;
+        public Monster(int id)
+        {
+            this.id = id;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Dictionary<int, Monster> dic = new Dictionary<int, Monster>();
+            for(int i =0; i< 10000; i++)
+            {
+                dic.Add(i, new Monster(i));
+            }
+
+            Monster monster1000;
+            bool found1000 = dic.TryGetValue(1000, out monster1000);
+        }
+    }
+}

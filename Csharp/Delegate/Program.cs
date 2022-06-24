e3d680384778f137c44b0f11b008b138d72742ea
Hoshi03 <didqocn114@hanyang.ac.kr>
2022-06-24 20:04:55 +0900

    함수를 담아서 넘겨줌

---
+++ Csharp/Delegate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        delegate int Onclicked();
+        //반환은 int고 입력은 void
+        //Onclicked는 이 delegate의 이름
+
+        static void Btnpressde(Onclicked clikedfunction)
+        {
+            clikedfunction();
+        }
+
+
+        //delegate에서 반환할 함수
+        static int DelegateTest()
+        {
+            Console.WriteLine("delegate");
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Btnpressde(DelegateTest);
+        }
+    }
+}

13ee47ef26f451bce47ca9990bc8f28dd85d1da0
Hoshi03 <didqocn114@hanyang.ac.kr>
2022-06-24 20:04:07 +0900

    get/set으로 인자 관리?

---
+++ Csharp/Property/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Property
+{
+    class Program
+    {
+
+       class Knight
+        {
+             
+            // 자동화 프로퍼티로 Mp 100 만들어 두고 게터, 세터 둘다 있는 버전
+            public int Mp { get; set; } = 100;
+        }
+
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+            knight.Mp = 50;
+            int a = knight.Mp;
+            Console.WriteLine(a);
+        }
+    }
+}

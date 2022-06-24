bb093cf55b50839f29c0cd692dbdce2d59d2c11d
Hoshi03 <didqocn114@hanyang.ac.kr>
2022-06-24 20:05:33 +0900

    조건 있는 델리게이트

---
+++ Csharp/Event/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event
+{
+    class InputManager
+    {
+        public delegate void Oninputkey();
+        public event Oninputkey Inputkey;
+        public void Update()
+        {
+            if (Console.KeyAvailable == false)
+                return;
+            ConsoleKeyInfo info =  Console.ReadKey();
+            if(info.Key == ConsoleKey.A)
+            {
+                Inputkey();
+            }
+        }   
+    }
+    class Program
+    {
+        static void Oninputtest()
+        {
+            Console.WriteLine("input 확인!");
+        }
+        static void Main(string[] args)
+        {
+            InputManager input = new InputManager();
+            input.Inputkey += Oninputtest;
+
+            while (true)
+            {
+                input.Update();
+            }
+        }
+    }
+}
